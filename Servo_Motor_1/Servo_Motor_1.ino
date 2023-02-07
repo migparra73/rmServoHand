13bafe53cb46f3b657763385b2cd026655b53b31
Miguel Parra <migparra73@gmail.com>
2023-02-06 17:48:51 -0800

    Fixed the motorIdx and motorId bug. Renamed

--- Servo_Motor_1/Servo_Motor_1.ino
+++ Servo_Motor_1/Servo_Motor_1.ino
@@ -60,29 +60,63 @@ Dynamixel2Arduino dxl(Serial, DXL_DIR_PIN); // When initializing the dxl class, 
 
 typedef struct
 {
-  uint8_t motorId;
+  uint8_t motorName;
   motorType_e type;
   motorDirection_e direction;
 } motorValues_t;
 
-motorValues_t motors[6] = { {1, DXL_M181, reverseDirection},
+#define NUMBER_OF_MOTORS 6
+
+motorValues_t motors[NUMBER_OF_MOTORS] = { {1, DXL_M181, normalDirection},
                             {2, DXL_M288, normalDirection},
                             {3, DXL_M288,  normalDirection},
                             {4, DXL_M181,  normalDirection},
                             {5, DXL_M288,  normalDirection},
                             {6, DXL_M181,  normalDirection}};
 
-
-bool _setPosition(Dynamixel2Arduino *obj, uint8_t motorId, float desiredAngle)
+typedef struct
 {
-  return obj->setGoalPosition(motorId, desiredAngle, UNIT_DEGREE);
+  float homeAngle;
+  float straightAngle;
+  float bentOutAngle;
+} motorTranslationValues_t;
+
+motorTranslationValues_t translations[NUMBER_OF_MOTORS] = {
+  {180.0, 90.0, 0.0},
+  {180.0, 270.0, 360.0},
+  {360.0, 270.0, 180.0},
+  {270.0, 180.0, 90.0},
+  {180.0, 270.0, 360.0},
+  {90.0, 180.0, 270.0}};
+
+
+float translateAngle(uint8_t tableIdx, float desiredAngle)
+{
+  float realAngle = 0.0;
+
+  // Example is motor #3
+  // If home is 360 and out is 180, that means that we should map 0 -> 360 and 180 to 180.
+  if(translations[tableIdx].homeAngle > translations[tableIdx].bentOutAngle)
+  {
+    realAngle = (-0.5*(desiredAngle)) + translations[tableIdx].homeAngle;
+  }
+  else
+  {
+    realAngle = (0.5*(desiredAngle)) + translations[tableIdx].homeAngle;
+  }
+}
+
+
+bool _setPosition(Dynamixel2Arduino *obj, uint8_t motorName, float desiredAngle)
+{
+  return obj->setGoalPosition(motorName, desiredAngle, UNIT_DEGREE);
 }
 
 #define END_OF_EEPROM_AREA_REGISTERS 64 // this tells us which register is the end of the eeprom area.
 // This function will make sure that we can write safely to the control table, based on if the torque enable bit is true.
-bool MotorControl_WriteControlTable(Dynamixel2Arduino *obj, uint8_t item_idx, uint8_t motorId, uint32_t data)
+bool MotorControl_WriteControlTable(Dynamixel2Arduino *obj, uint8_t item_idx, uint8_t motorName, uint32_t data)
 {
-  bool torqueEnable = obj->getTorqueEnableStat(motorId);
+  bool torqueEnable = obj->getTorqueEnableStat(motorName);
   if (item_idx < END_OF_EEPROM_AREA_REGISTERS)
   {
     if(torqueEnable)
@@ -93,25 +127,33 @@ bool MotorControl_WriteControlTable(Dynamixel2Arduino *obj, uint8_t item_idx, ui
     }
     else
     {
-      obj->writeControlTableItem(item_idx,motorId, data);
+      obj->writeControlTableItem(item_idx,motorName, data);
       return true;
     }
   }
   else
   {
       // Value is greater, can always write.
-      obj->writeControlTableItem(item_idx,motorId, data);
+      obj->writeControlTableItem(item_idx,motorName, data);
       return true;    
   }
 }
 
-bool MotorControl_SetPosition(Dynamixel2Arduino *obj, uint8_t motorId, float desiredAngle, hardwareErrorStatus_t *hardwareErrorStatus){
-  hardwareErrorStatus->all = 0;
-  if(!_setPosition(obj, motorId, desiredAngle))
+
+// Top level handler for handling indexes instead of Ids
+bool MotorControl_SetPositionWrapper(Dynamixel2Arduino *obj, uint8_t tableIdx, float desiredAngle, hardwareErrorStatus_t *hardwareErrorStatus)
+{
+  float actualAngle = 0.0;
+  actualAngle = translateAngle(tableIdx, desiredAngle);
+  MotorControl_SetPosition(obj, motors[tableIdx].motorName, actualAngle, hardwareErrorStatus);
+}
+
+bool MotorControl_SetPosition(Dynamixel2Arduino *obj, uint8_t motorName, float desiredAngle, hardwareErrorStatus_t *hardwareErrorStatus){
+  if(!_setPosition(obj, motorName, desiredAngle))
   {
     // this should be a function call.
     delay(1000);
-    hardwareErrorStatus->all = obj->readControlTableItem(HARDWARE_ERROR_STATUS, motorId); // read HW error status register
+    hardwareErrorStatus->all = obj->readControlTableItem(HARDWARE_ERROR_STATUS, motorName); // read HW error status register
     // Motor didn't work, let's find out why.
     Serial1.write((char *)hardwareErrorStatus->all, sizeof(uint8_t));
     return false;
@@ -120,31 +162,31 @@ bool MotorControl_SetPosition(Dynamixel2Arduino *obj, uint8_t motorId, float des
 }
 
 
-bool setupMotor(Dynamixel2Arduino *obj, uint8_t motorIdx, OperatingMode opMode, motorDirection_e direction)
+bool setupMotor(Dynamixel2Arduino *obj, uint8_t motorName, OperatingMode opMode, motorDirection_e direction, motorType_e motorType)
 {
   bool success = false;
-  success = obj->torqueOff(motors[motorIdx].motorId);
-  success &= obj->setOperatingMode(motors[motorIdx].motorId, opMode); // EEPROM AREA
-  success &= MotorControl_WriteControlTable(obj, DRIVE_MODE, motors[motorIdx].motorId, (uint8_t) direction); // EEPROM AREA
-  success &= MotorControl_WriteControlTable(obj, HOMING_OFFSET, motors[motorIdx].motorId, 0);
-  if(DXL_M181 == motors[motorIdx].type)
+  success = obj->torqueOff(motorName);
+  success &= obj->setOperatingMode(motorName, opMode); // EEPROM AREA
+  success &= MotorControl_WriteControlTable(obj, DRIVE_MODE, motorName, (uint8_t) direction); // EEPROM AREA
+  success &= MotorControl_WriteControlTable(obj, HOMING_OFFSET, motorName, 0);
+  if(DXL_M181 == motorType)
   {
-    MotorControl_WriteControlTable(&dxl, MAX_POSITION_LIMIT, motors[motorIdx].motorId, DXL_M181_MAX_POS_LIMIT);
-    MotorControl_WriteControlTable(&dxl, MIN_POSITION_LIMIT, motors[motorIdx].motorId, DXL_M181_MIN_POS_LIMIT);
+    MotorControl_WriteControlTable(&dxl, MAX_POSITION_LIMIT, motorName, DXL_M181_MAX_POS_LIMIT);
+    MotorControl_WriteControlTable(&dxl, MIN_POSITION_LIMIT, motorName, DXL_M181_MIN_POS_LIMIT);
   }
-  else if(DXL_M288 == motors[motorIdx].type)
+  else if(DXL_M288 == motorType)
   {
-    MotorControl_WriteControlTable(&dxl, MAX_POSITION_LIMIT, motors[motorIdx].motorId, DXL_M288_MAX_POS_LIMIT);
-    MotorControl_WriteControlTable(&dxl, MIN_POSITION_LIMIT, motors[motorIdx].motorId, DXL_M288_MIN_POS_LIMIT);    
+    MotorControl_WriteControlTable(&dxl, MAX_POSITION_LIMIT, motorName, DXL_M288_MAX_POS_LIMIT);
+    MotorControl_WriteControlTable(&dxl, MIN_POSITION_LIMIT, motorName, DXL_M288_MIN_POS_LIMIT);    
   }
-  success &= obj->torqueOn(motors[motorIdx].motorId);
+  success &= obj->torqueOn(motorName);
   return success;
 }
 
-bool checkIfMotorAlive(Dynamixel2Arduino *obj, uint8_t motorId)
+bool checkIfMotorAlive(Dynamixel2Arduino *obj, uint8_t motorName)
 {
   // Check to see if the Dynamixel is alive
-  if(obj->ping(motorId))
+  if(obj->ping(motorName))
   {
     Serial1.println("Dynamixel detected");
   }
@@ -154,19 +196,19 @@ bool checkIfMotorAlive(Dynamixel2Arduino *obj, uint8_t motorId)
   }
 }
 
-bool MotorControl_Init(Dynamixel2Arduino *obj, uint8_t motorIdx)
+// THIS FUNCTION USES INDEX, NOT THE MOTOR ID.
+bool MotorControl_Init(Dynamixel2Arduino *obj, uint8_t tableIdx)
 {  
-  checkIfMotorAlive(&dxl, motors[motorIdx].motorId);
-  setupMotor(obj, motors[motorIdx].motorId, OP_POSITION, motors[motorIdx].direction); // Solo finger (Motor 1)
+  checkIfMotorAlive(&dxl, motors[tableIdx].motorName);
+  setupMotor(obj, motors[tableIdx].motorName, OP_POSITION, motors[tableIdx].direction, motors[tableIdx].type); // Solo finger (Motor 1)
 }
 
-void setup() {
-  const int numberOfMotors = (sizeof(motors) / sizeof(motorValues_t));
+void setup() 
+{
+  const int numberOfMotors = NUMBER_OF_MOTORS;
   Serial1.begin(HOST_COMMUNICATION_BAUDRATE); // Sets up the external serial port (USB dongle thing)
   dxl.begin(DYNAMIXEL_COMMUNICATION_BAUDRATE); //  Sets up communication with Dynamixel servos 
   dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
-  
-
 
   for(int i = 0; i < numberOfMotors; i++)
   {
@@ -174,16 +216,14 @@ void setup() {
   }
  
   hardwareErrorStatus_t hwStatus;
-  // MotorControl_SetPosition(&dxl, 5, 270.0, &hwStatus);
-    
+  MotorControl_SetPosition(&dxl, 2, 270.0, &hwStatus);
 }
 
 
 
 void loop() {
-
   float desiredAngle = 0.0; // apparently float and double have the same size on arduino, 4 bytes.
-  uint32_t motorId = 2;
+  uint8_t motorName = 1;
   hardwareErrorStatus_t hwStatus;
   int i;
 
@@ -195,34 +235,8 @@ void loop() {
     Serial1.write((char*)&desiredAngle, sizeof(desiredAngle));
     Serial1.println("");
 
-    if(MotorControl_SetPosition(&dxl, motorId, desiredAngle, &hwStatus))
-    {
-      Serial1.println("Success!");
-    }
-    else
-    {
-      if(hwStatus.bits.electricalShock)
-      {
-        Serial1.println("Fail: Electrical shock detected");
-      }
-      else if(hwStatus.bits.overHeating)
-      {
-        Serial1.println("Fail: Overheating detected");        
-      }
-      else if(hwStatus.bits.overloadError)
-      {
-        Serial1.println("Fail: Overload error detected");        
-      }
-      else if(hwStatus.bits.voltageError)
-      {
-        Serial1.println("Fail: Voltage error detected");        
-      }
-      else
-      {
-        Serial1.println("Fail: Unknown error occured.");                
-      }
-      dxl.reboot(motorId);
-    }
+  //if(MotorControl_SetPositionWrapper(&dxl, motorName-1, desiredAngle, &hwStatus))
+  MotorControl_SetPosition(&dxl, motorName, desiredAngle, &hwStatus);
   }
 }
 
